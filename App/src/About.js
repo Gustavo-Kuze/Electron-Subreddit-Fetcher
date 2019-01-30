2c4b2118347f15bade94b415bf20e1daee81effc
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-01-30 15:51:23 -0200

    Added comments on how to programatically minimize window (About)

--- App/src/About.js
+++ App/src/About.js
@@ -5,10 +5,12 @@ export default class About extends Component {
 
   openWebSite = () => {
     window.require("electron").shell.openExternal("https://www.gustavokuze.com")
+    // const { remote } = window.require('electron')
+    // remote.BrowserWindow.getFocusedWindow().minimize();
   }
 
   render() {
-    return (  
+    return (
       <div className="about-container">
         <p className="about-by">Developed by <span className="about-by-link" onClick={this.openWebSite}>Gustavo Kuze</span></p>
       </div>
